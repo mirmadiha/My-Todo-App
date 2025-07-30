e91d173973c57a272e6978769a12a3de4dc06262
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-07-30 23:58:50 +0530

    useState

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {useState} from "react";
+function App(){
+    const[task,setTask]=useState([]);
+    
+    return (
+        <div>
+            App
+        </div>
+    )
+}
+
+export default App;
